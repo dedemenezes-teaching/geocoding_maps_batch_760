06a19d2fe48c3c9e6e5c82f90c54f45e9eb86804
andre menezes <djdedemenezes@gmail.com>
2021-11-24 10:09:40 -0300

    added custom markers to map and info window

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
 };
 
 const initMapbox = () => {
@@ -19,8 +19,19 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(map);
     });
 
